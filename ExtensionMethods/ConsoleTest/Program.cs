c5cdfd317cade2991ee9bbc0cc00cd1d54be3dc9
Tsovinar <Tsovinar.ghazaryan@yahoo.com>
2017-01-17 01:09:46 +0400

    Extention methods

---
+++ ExtensionMethods/ConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Anna";
+            int value1 = -13;
+            int value2 = -28;
+            int value3 = 28; 
+            Console.WriteLine(a.IsPolindrom(true));
+            Console.WriteLine(value1.SumOfDigets());
+            Console.WriteLine(value1.IsPrime());
+            Console.WriteLine(value2.IsPerfect());
+            Console.WriteLine(value3.IsPerfect());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsPolindrom(this string str, bool ignorUpperCase)
+        {
+            string temp;
+            if (ignorUpperCase)
+            {
+                temp = str.ToLower();
+            }
+            else
+            {
+                temp = str;
+            }
+            var strbld = new StringBuilder();
+
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                strbld.Append(temp[i]);
+            }
+            return (temp == strbld.ToString());
+        }
+
+        public static int SumOfDigets(this int a)
+        {
+            int sum = 0;
+            while (a / 10 != 0)
+            {
+                sum = (sum + a % 10);
+                a = a / 10;
+            }
+            return (sum + a);
+        }
+
+        public static bool IsPrime(this int a)
+        {
+            int temp = a;
+            if (a == 0 || a == 1)
+                return false;
+            if (a < 0)
+            {
+                temp = a * (-1);
+            }
+            for (int j = 2; j <= temp / 2; j++)
+            {
+                if (a % j == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        //Կատարյալ թիվ, այն բնական թիվն է, որը հավասար է իրենից բացի, իր բոլոր բաժանարարների գումարին։
+        public static bool IsPerfect(this int num)
+        {
+            int sum = 0;
+            if (num == 0 || num == 1)
+                return false;
+            else
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum + 1 == num)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+
+    }
+
+}
+
